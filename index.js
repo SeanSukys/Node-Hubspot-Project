3a69f70022ab23488363adf0c980fbc411c87aa5
ssukys09 <sean.sukys@gmail.com>
2022-08-18 14:51:01 -0400

    3 files modified

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// run `node index.js` in the termina
+// run `node index.js` in the terminal
 const hubspot = require('@hubspot/api-client');
 const request = require('request-promise');
+const moment = require('moment');
 require('dotenv').config();
+
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const hubspotClient = new hubspot.Client({ accessToken: ACCESS_TOKEN });
 
-(async function(){
- const endpoint = 
-  try{
-    await request({
-      method: GET,
-      url: `https:api.hubspot.com`,
-      qs
+async function getContacts() {
+  const response = await hubspotClient.apiRequest({
+    method: 'GET',
+    path: '/crm/v3/objects/contacts',
+  });
+  const json = await response.json();
+  console.log(json);
+}
 
-    })
-  }
-catch(e){console.log("This is an error:",e)}
-
-
-})();
+getContacts();
