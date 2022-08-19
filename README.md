25499c167f6c262eb2a15d80c531d4a9820d01a5
ssukys09 <sean.sukys@gmail.com>
2022-08-19 10:31:06 -0400

    3 files modified

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# node-g4dtnx
+# Node-Hubspot Project
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-g4dtnx)
+
+1. Get the Hubspot API Node Library here: https://www.npmjs.com/package/@hubspot/api-client
+
+2. or run in the CLI: npm i @hubspot/api-client
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//This lets us store our access token as an environment variable. I am hiding it from GIT for security.
 require('dotenv').config();
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+//-------------------------------------------------------------------
 
-console.log(process.env);
+//Lets instantiate a new hubspot client utilizing the API Library
+const hubspot = require('@hubspot/api-client');
+const hubspotClient = new hubspot.Client({ accessToken: ACCESS_TOKEN });
+
+//Test Functions
+function sayHello() {
+  console.log('Hello!');
+}
+
+sayHello();
