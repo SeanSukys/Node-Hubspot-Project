9d07747b591471e7a2999f3ea0d9cb102886eadd
ssukys09 <sean.sukys@gmail.com>
2022-08-19 09:31:20 -0400

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-const hubspot = require('@hubspot/api-client');
-const hubspotClient = new hubspot.Client({
-  accessToken: 'pat-na1-016960fb-4f4b-406d-97e8-5755bf821892',
-});
+require('dotenv').config();
 
-const filter = {
-  propertyName: 'createdate',
-  operator: 'GTE',
-  value: '2021-04-21T18:12:42.678Z',
-};
-const filterGroup = { filters: [filter] };
-const sort = JSON.stringify({
-  propertyName: 'createdate',
-  direction: 'DESCENDING',
-});
-const query = 'test';
-const properties = ['createdate', 'firstname', 'lastname'];
-const limit = 100;
-const after = 0;
-
-const publicObjectSearchRequest = {
-  limit: '5',
-  properties: ['firstname', 'lastname'],
-};
-async function apiCall() {
-  const result = await hubspotClient.crm.contacts.searchApi.doSearch(
-    publicObjectSearchRequest
-  );
-  console.log(JSON.stringify(result));
-}
-
-apiCall();
+console.log(process.env);
